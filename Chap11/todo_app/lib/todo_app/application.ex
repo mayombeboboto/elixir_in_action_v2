ac7d9c460c58cf24f7b17b6e9b14eb1986d0e076
mayombeboboto <mayombegradi@gmail.com>
2023-08-12 09:11:54 +0200

    Elixir Application Demo

---
+++ Chap11/todo_app/lib/todo_app/application.ex
@@ -0,0 +1,10 @@
+defmodule TodoApp.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args), do: Todo.System.start_link()
+end
